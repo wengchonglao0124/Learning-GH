75c2141846f5307d3b5cf6ef1a8ff37be28f5059
Weng Chong <wengchonglao0124@gmail.com>
2022-07-09 23:37:26 +1000

    Initial Commit

---
+++ LearningGitHub/LearningGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGitHubApp.swift
+//  LearningGitHub
+//
+//  Created by weng chong lao on 9/7/2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
